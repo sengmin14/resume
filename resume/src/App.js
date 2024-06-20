9352e2b6d33a60eaa3ad7d55453928db711c474c
진성민 <sengmin14@naver.com>
2024-06-20 12:26:10 +0900

    feat : react Test

---
+++ resume/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>깃허브 테스트</h1>
+        </div>
+    );
+}
+
+export default App;
