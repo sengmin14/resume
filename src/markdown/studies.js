f37cb2785f28e0d81210541f3b68fd3482ee40c1
진성민 <sengmin14@naver.com>
2024-07-15 20:35:29 +0900

    오타 수정

--- src/markdown/studies.js
+++ src/markdown/studies.js
@@ -5,14 +5,18 @@ export const studies = [
     },
     {
         id: 1,
-        repo: 'sengmin14/JAVA_Study',
+        repo: 'sengmin14/reading-books-for-spring',
     },
     {
         id: 2,
-        repo: 'sengmin14/CS-Study',
+        repo: 'sengmin14/JAVA_Study',
     },
     {
         id: 3,
+        repo: 'sengmin14/CS-Study',
+    },
+    {
+        id: 4,
         repo: 'sengmin14/Algorithm',
     },
 ];
