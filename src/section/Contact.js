f3bfbe6d408fd33bb616c83ca869d821781cacaa
Jin SeongMin <sengmin14@naver.com>
2025-12-23 17:45:23 +0900

    error : 빈 <a> 태그 제거

--- src/section/Contact.js
+++ src/section/Contact.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import SectionTitle from '../components/SectionTitle';
+import React from "react";
+import SectionTitle from "../components/SectionTitle";
 
 function Contact({ scrollRef }) {
-    return (
-        <div
-            ref={(cur) => {
-                if (cur) {
-                    scrollRef.current[1] = cur;
-                }
-            }}
-        >
-            <SectionTitle>Contact</SectionTitle>
-            <table>
-                <tbody>
-                    <tr>
-                        <th>Email</th>
-                        <td>
-                            <a href="mailto:sengmin14@naver.com">sengmin14@naver.com</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Github</th>
-                        <td>
-                            <a href="https://github.com/sengmin14">github.com/sengmin14</a>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th>Blog</th>
-                        <td>
-                            <a></a>
-                        </td>
-                    </tr>
-                    {/* <tr>
+  return (
+    <div
+      ref={(cur) => {
+        if (cur) {
+          scrollRef.current[1] = cur;
+        }
+      }}
+    >
+      <SectionTitle>Contact</SectionTitle>
+      <table>
+        <tbody>
+          <tr>
+            <th>Email</th>
+            <td>
+              <a href="mailto:sengmin14@naver.com">sengmin14@naver.com</a>
+            </td>
+          </tr>
+          <tr>
+            <th>Github</th>
+            <td>
+              <a href="https://github.com/sengmin14">github.com/sengmin14</a>
+            </td>
+          </tr>
+          <tr>
+            <th>Blog</th>
+            <td></td>
+          </tr>
+          {/* <tr>
                 <th>Instagram</th>
                 <td>
                     <a href="https://www.instagram.com/dain.py/">
@@ -39,10 +37,10 @@ function Contact({ scrollRef }) {
                     </a>
                 </td>
             </tr> */}
-                </tbody>
-            </table>
-        </div>
-    );
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Contact;
